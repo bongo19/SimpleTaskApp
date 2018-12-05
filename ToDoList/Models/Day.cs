b08ce03430c2f2d178e06e56205b2b031f93887b
Bongo, Felicity <FBongo@RSCS.COM>
2018-12-05 11:52:02 -0500

    Adding Entity Models and setting up connection string

---
+++ ToDoList/Models/Day.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models
+{
+    public class Day
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(25)]
+        public string TaskDay { get; set; }
+
+        public ICollection<ToDoItem> Tasks { get; set; } = new List<ToDoItem>();
+    }
+}
---
+++ ToDoList/Models/ToDoItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ToDoList.Models
+{
+    public class ToDoItem
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string TaskName { get; set; }
+
+        [MaxLength(500)]
+        public string TaskDescription { get; set; }
+
+        public int TaskDay { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
